0e5325ae03aef56daf45a03b0e30cefef6875527
Roman Gontar <ashenoooone@gmail.com>
2024-05-10 12:07:53 +0400

    feat добавил квери клиент + примеры для мутации и для квери

--- src/shared/api/types.d.ts
+++ src/shared/api/types.d.ts
@@ -37,3 +37,71 @@ type BaseError = {
 };
 
 type ApiError = AxiosError<BaseError>;
+
+// ~~~ ПРИМЕР МУТАЦИИ ~~~
+
+// type PostCreateStrategyParams = {
+//   name: string;
+// };
+
+// export type PostCreateStrategyRequestConfig =
+//   RequestConfig<PostCreateStrategyParams>;
+
+// export type PostCreateStrategyResponse = {
+//   name: string;
+//   is_active: boolean;
+//   sources: any[];
+//   id: number;
+// };
+
+// export const postCreateStrategy = ({
+//   params,
+//   config,
+// }: PostCreateStrategyRequestConfig) =>
+//   $api.post<PostCreateStrategyResponse>(
+//     "/settings",
+//     { ...params, sources: [] },
+//     config
+//   );
+
+// export const usePostCreateStrategyMutation = (
+//   settings?: MutationSettings<
+//     PostCreateStrategyRequestConfig,
+//     typeof postCreateStrategy
+//   >
+// ) => {
+//   const queryClient = useQueryClient();
+//   return useMutation({
+//     mutationKey: ["postCreateStrategy"],
+//     onSettled: async () => {
+//       await queryClient.invalidateQueries({
+//         queryKey: [USER_SETTINGS_QUERY_KEY],
+//       });
+//     },
+//     mutationFn: ({ params, config }) =>
+//       postCreateStrategy({
+//         params,
+//         config: { ...settings?.config, ...config },
+//       }),
+//     ...settings?.options,
+//   });
+// };
+
+// ~~~ ПРИМЕР КВЕРИ ~~~
+
+// type GetSettingsResponse = UserSettingsType[];
+
+// export const getSettings = () => $api.get<GetSettingsResponse>("/settings");
+
+// const STALE_TIME = 1000 * 60 * 5; // 5 minutes
+
+// export const USER_SETTINGS_QUERY_KEY = "settings";
+
+// export function useGetSettings(settings?: QuerySettings<typeof getSettings>) {
+//   return useQuery({
+//     staleTime: STALE_TIME,
+//     queryFn: () => getSettings(),
+//     queryKey: [USER_SETTINGS_QUERY_KEY],
+//     ...settings,
+//   });
+// }
