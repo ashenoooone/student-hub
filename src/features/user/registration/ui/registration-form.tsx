4261f927e6a9f8f1c4e36afc17a180d5ce211f97
Roman Gontar <ashenoooone@gmail.com>
2024-05-11 11:46:36 +0400

    fix в реге небоьлшие фиксы + обработак ошибок

--- src/features/user/registration/ui/registration-form.tsx
+++ src/features/user/registration/ui/registration-form.tsx
@@ -1,33 +1,51 @@
-import {Typography} from "@/shared/ui/typography";
-import {Button} from "@/shared/ui/button";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle,} from "@/shared/ui/card";
-import {Input} from "@/shared/ui/input";
-import {cn} from "@/shared/utils";
+import { Typography } from "@/shared/ui/typography";
+import { Button } from "@/shared/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/shared/ui/card";
+import { Input } from "@/shared/ui/input";
+import { cn } from "@/shared/utils";
 import Link from "next/link";
-import {ROUTES} from "@/shared/conts";
-import {FormEventHandler} from "react";
-import {UsersService} from "@/entities/user";
-import {PostRegisterUserParams} from "@/entities/user/model/types";
-import {useUserStore} from "@/entities/user/model/store";
+import { ROUTES } from "@/shared/conts";
+import { FormEventHandler } from "react";
+import { UsersService } from "@/entities/user";
+import { PostRegisterUserParams } from "@/entities/user/model/types";
+import { useUserStore } from "@/entities/user/model/store";
+import { isApiError } from "@/shared/api/utils";
+import { useToast } from "@/shared/ui/use-toast";
 
 type RegistrationFormProps = {
   className?: string;
 };
 
 export const RegistrationForm = (props: RegistrationFormProps) => {
-  const {className} = props;
+  const { className } = props;
+  const setUser = useUserStore.use.setUser();
+  const { toast } = useToast();
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     Object.fromEntries(formData);
     try {
-      const {data: tokens} = await UsersService.instance.register(Object.fromEntries(formData) as PostRegisterUserParams);
-      useUserStore.use.setUser()(tokens);
+      const { data: tokens } = await UsersService.instance.register(
+        Object.fromEntries(formData) as PostRegisterUserParams
+      );
+      setUser(tokens);
     } catch (e) {
-      console.error(e);
+      if (isApiError(e)) {
+        toast({
+          title: "Произошла ошибка",
+          description: e.response?.data?.message,
+          variant: "destructive",
+        });
+      }
     }
-  }
+  };
 
   return (
     <Card className={cn("", className)}>
@@ -37,9 +55,30 @@ export const RegistrationForm = (props: RegistrationFormProps) => {
       </CardHeader>
       <CardContent>
         <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-          <Input name='login' label="Логин" placeholder="Логин" type="text" required minLength={4}/>
-          <Input name='email' label="Почта" placeholder="Почта" type="email" required minLength={5}/>
-          <Input name='password' label="Пароль" placeholder="Пароль" type="password" required minLength={3}/>
+          <Input
+            name="login"
+            label="Логин"
+            placeholder="Логин"
+            type="text"
+            required
+            minLength={4}
+          />
+          <Input
+            name="email"
+            label="Почта"
+            placeholder="Почта"
+            type="email"
+            required
+            minLength={5}
+          />
+          <Input
+            name="password"
+            label="Пароль"
+            placeholder="Пароль"
+            type="password"
+            required
+            minLength={3}
+          />
           <Button className="mt-4">Зарегистрироваться</Button>
           <Typography>
             Есть аккаунт?
