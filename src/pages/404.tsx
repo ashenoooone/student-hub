1c835830a15a53f90ad07da878d6b0891bf6575a
Roman Gontar <ashenoooone@gmail.com>
2024-05-12 20:38:08 +0400

    feat 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,31 @@
-const NotFount = () => {
+import NotFoundImage from "@/shared/assets/Error_404.svg";
+import { Box } from "@/shared/ui/box";
+import { Button } from "@/shared/ui/button";
+import { Page } from "@/shared/ui/page";
+import { Typography } from "@/shared/ui/typography";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  const onBackClick = () => {
+    router.back();
+  };
+
   return (
-    <div>
-      404 - Not Found Custom page
-    </div>
+    <Page>
+      <Box className="flex flex-col items-center justify-center">
+        <Image alt="Не найдено" src={NotFoundImage} />
+        <Typography variant={"h2"} className="mt-2">
+          Страница не найдена или находится в разработке
+        </Typography>
+        <Button onClick={onBackClick} className="mt-2">
+          Вернуться назад
+        </Button>
+      </Box>
+    </Page>
   );
 };
 
-export default NotFount
+export default NotFound;
