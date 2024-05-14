0d83c2ecd9b897591b4c18e5de3958e33fb50c7f
Roman Gontar <ashenoooone@gmail.com>
2024-05-14 21:41:40 +0400

    feat мероприятие дня на главной странице

--- src/entities/events/ui/top-event.tsx
+++ src/entities/events/ui/top-event.tsx
@@ -4,6 +4,10 @@ import { cn } from "@/shared/utils";
 import React from "react";
 import { EventType } from "../model/types";
 import { Button } from "@/shared/ui/button";
+import Link from "next/link";
+import { ROUTES } from "@/shared/conts";
+import Image from "next/image";
+import { MembersList } from "@/shared/ui/members";
 
 type TopEventProps = {
   className?: string;
@@ -16,22 +20,48 @@ export const TopEvent = React.memo((props: TopEventProps) => {
   return (
     <Box
       className={cn(
-        "bg-gradient-to-r text-white from-blue-600 to-violet-600",
+        "bg-gradient-to-r flex gap-3 text-white from-blue-600 to-violet-600",
         className
       )}
     >
-      <Typography className="uppercase mb-2" variant={"h2"}>
-        Мероприятие дня
-      </Typography>
-      <Typography className="" variant={"h2"}>
-        {event?.name}
-      </Typography>
-      <Typography className="font-medium line-clamp-[12] w-1/2">
-        {event?.description}
-      </Typography>
-      <Button variant={"outline"} className="text-black mt-4">
-        Узнать больше
-      </Button>
+      <div className="w-1/2 flex flex-col">
+        <Typography className="uppercase mb-2" variant={"h2"}>
+          Мероприятие дня
+        </Typography>
+        <Typography className="" variant={"h2"}>
+          {event?.name}
+        </Typography>
+        <Typography className="font-medium line-clamp-[12]">
+          {event?.description}
+        </Typography>
+        <Button variant={"outline"} className="text-black mt-auto">
+          <Link href={`${ROUTES.events}/${event?.id}`}>Узнать больше</Link>
+        </Button>
+      </div>
+      <div className="">
+        {event && event.avatar && (
+          <Image
+            width={1000}
+            height={1000}
+            src={event?.avatar}
+            alt={event?.name}
+            className="max-w-[260px] max-h-[260px] rounded"
+          />
+        )}
+        {event?.members && (
+          <div className="mt-2">
+            <Typography className="font-bold">Уже участвуют: </Typography>
+            <MembersList
+              totalMembers={event.membersCount}
+              members={event?.members.map((m) => ({
+                avatar: m.avatar,
+                link: `${ROUTES.profile}/${m.id}`,
+                name: m.name,
+              }))}
+            />
+          </div>
+        )}
+      </div>
     </Box>
   );
 });
