c37f357698e7d51763833e86bc13110c2b7c9681
tyferly <tyferly2003@gmail.com>
2024-05-15 01:32:30 +0400

    feat додедал карточки юзера

--- src/entities/user/ui/card-user.tsx
+++ src/entities/user/ui/card-user.tsx
@@ -4,6 +4,9 @@ import {Avatar, AvatarFallback} from "@/shared/ui/avatar";
 import Image from "next/image";
 import {Badge} from "@/shared/ui/badge";
 import {Compliment} from "@/pages-composite/profile-page/ui/compliment";
+import ComplimentMini from "@/entities/user/ui/compliment";
+import {Typography} from "@/shared/ui/typography";
+import {ROUTES} from "@/shared/conts";
 
 type Props = {
     user: UserType
@@ -13,8 +16,8 @@ const CardUser = (props: Props) => {
     const { user } = props
 
     return (
-        <div className={"bg-white p-5 rounded-2xl flex flex-row items-start gap-4"}>
-            <Avatar className={"w-[100px] h-[100px]"}>
+        <div className={"bg-white p-5 rounded-2xl flex flex-row items-start gap-4 hover:scale-[101%] transition"}>
+            <Avatar className={"w-[100px] h-[100px] cursor-pointer"} link={`${ROUTES.profile}/${user.id}`}>
                 {user.avatar ? (
                     <Image src={user.avatar} alt={user.login} width={1000} height={1000} className={"w-[100px] h-[100px]"}/>
                 ) : (
@@ -30,13 +33,30 @@ const CardUser = (props: Props) => {
                                 <Badge className={'w-max max-w-[83px] text-ellipsis text-[12px] hover:bg-blue-400 select-none'} key={index}>{role.name}</Badge>
                             ))}
                     {user.rolesForProject.length > 2 && <Badge className={'w-max hover:bg-blue-400 select-none'}>...</Badge>}
+                    {user.rolesForProject.length === 0 && <Typography affects={"muted"}>Роли отсутствуют</Typography>}
                 </div>
-                <div className={"flex flex-row gap-2 mt-2"}>
-                    <Compliment
+                <div className={"flex flex-row gap-2 mt-2 "}>
+                    <ComplimentMini
                         icon={"🤝"}
                         text={UserRatingMapper.politeness}
-                        count={user.ratings.politeness}
+                        count={user.ratings?.politeness ?? 0}
                     />
+                    <ComplimentMini
+                        icon={"✨"}
+                        text={UserRatingMapper.creativity}
+                        count={user.ratings?.politeness ?? 0}
+                    />
+                    <ComplimentMini
+                        icon={"📚"}
+                        text={UserRatingMapper.learningAbility}
+                        count={user.ratings?.politeness ?? 0}
+                    />
+                    <ComplimentMini
+                        icon={"🌷"}
+                        text={UserRatingMapper.responsibility}
+                        count={user.ratings?.politeness ?? 0}
+                    />
+
                 </div>
             </div>
         </div>
--- src/entities/user/ui/compliment.tsx
+++ src/entities/user/ui/compliment.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 
-const Compliment = () => {
+type Props = {
+    icon: ReactNode;
+    text: string
+    count: number
+}
+
+const ComplimentMini = (props: Props) => {
+    const {icon, text, count} = props
     return (
-        <div>
-            
+        <div className={"flex flex-row gap-2 items-center bg-muted px-2 py-1 rounded-md cursor-pointer"}>
+            <div>{icon}</div>
+            <div className={"text-xs font-bold"}>{count}</div>
         </div>
     );
 };
 
-export default Compliment;
+export default ComplimentMini;
